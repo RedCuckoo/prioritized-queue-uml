c526c2f58f0913da67dd7791510ca3bc8ba943bb
RedCuckoo <momotenko.yuriy@gmail.com>
2020-04-16 14:50:12 +0300

    Added GUI classes

--- prioritized-queue-uml/prioritized-queue-uml-gui/prioritized-queue/mainwindow.cpp
+++ prioritized-queue-uml/prioritized-queue-uml-gui/prioritized-queue/mainwindow.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    lqueue = nullptr;
+    tqueue = nullptr;
+    vqueue = nullptr;
+    toggledRadioButton = ui->radioButtonAVLTree;
+    queueType = 1;
+    connect(ui->radioButtonList,&QRadioButton::toggled,this,&MainWindow::on_radioButton_toggled);
+    connect(ui->radioButtonAVLTree,&QRadioButton::toggled,this,&MainWindow::on_radioButton_toggled);
+    connect(ui->radioButtonVector,&QRadioButton::toggled,this,&MainWindow::on_radioButton_toggled);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,68 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+bool MainWindow::oneRadioButtonSelected(){
+    return toggledRadioButton;
+}
+
+void MainWindow::on_pushButtonAdd_clicked(){
+    if (oneRadioButtonSelected()){
+        if (ui->lineEditData->text() == ""){
+            QMessageBox* mbox = new QMessageBox(this);
+            mbox->setModal(true);
+            mbox->setText("Enter some value, please");
+        }
+        else{
+            switch(queueType){
+            case 0:
+                lqueue->push(ui->lineEditData->text().toStdString(), std::stoi(ui->comboBoxPriority->currentText().toStdString()));
+                break;
+            case 1:
+                tqueue->push(QString::fromStdString(ui->lineEditData->text()), std::stoi(ui->comboBoxPriority->currentText().toStdString()));
+                break;
+            case 2:
+                vqueue->push(ui->lineEditData->text(), std::stoi(ui->comboBoxPriority->currentText().toStdString()));
+                break;
+            }
+
+            updateOutput();
+        }
+    }
+    else{//TODO: del this
+        QMessageBox* mbox = new QMessageBox(this);
+        mbox->setModal(true);
+        mbox->setText("Please, choose the type of the queue first");
+    }
+}
+
+void MainWindow::on_radioButton_toggled(bool){
+    toggledRadioButton = static_cast<QRadioButton*>(sender());
+    updateQueueType();
+}
+
+void MainWindow::updateQueueType(){
+    if (toggledRadioButton == ui->radioButtonList)
+        queueType = 0;
+
+    if (toggledRadioButton == ui->radioButtonAVLTree)
+        queueType = 1;
+
+    if (toggledRadioButton == ui->radioButtonVector)
+        queueType = 2;
+
+    updateOutput();
+}
+
+void MainWindow::updateOutput(){
+    switch (queueType){
+    case 0:
+        ui->listWidget->addItem(QString::fromStdString(lqueue->to_string()));
+        break;
+    case 1:
+        ui->listWidget->addItem(QString::fromStdString(tqueue->to_string()));
+        break;
+    case 2:
+        ui->listWidget->addItem(QString::fromStdString(vqueue->to_string()));
+        break;
+    }
+}
--- prioritized-queue-uml/prioritized-queue-uml-gui/prioritized-queue/mainwindow.h
+++ prioritized-queue-uml/prioritized-queue-uml-gui/prioritized-queue/mainwindow.h
@@ -2,6 +2,10 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QRadioButton>
+#include "../../prioritized-queue-uml/priority_queue_data_structure/ListPriorQueue.h"
+#include "../../prioritized-queue-uml/priority_queue_data_structure/TreePriorQueue.h"
+#include "../../prioritized-queue-uml/priority_queue_data_structure/VectorPriorQueue.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +21,18 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    bool oneRadioButtonSelected();
+    ListPriorQueue<QString>* lqueue;
+    TreePriorQueue<QString>* tqueue;
+    VectorPriorQueue<QString>* vqueue;
+    //0 - lqueue 1 - tqueue 2 - vqueue
+    size_t queueType;
+    QRadioButton* toggledRadioButton;
+    void updateQueueType();
+    void updateOutput();
+private slots:
+    void on_pushButtonAdd_clicked();
+    void on_pushButtonChange_clicked();
+    void on_radioButton_toggled(bool);
 };
 #endif // MAINWINDOW_H
